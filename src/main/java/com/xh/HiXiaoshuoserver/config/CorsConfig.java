46b71a6efcdf57f02b002c968e29703aa0e842da
XuHao <286089659@qq.com>
2018-12-12 15:16:59 +0800

    修改接口

---
+++ src/main/java/com/xh/HiXiaoshuoserver/config/CorsConfig.java
@@ -0,0 +1,10 @@
+package com.xh.HiXiaoshuoserver.config;
+
+public class CorsConfig {
+}
+/**
+ * @program: HiXiaoshuo-server
+ * @description:
+ * @author: XuHao
+ * @create: 2018-12-12 10:29
+ **/
---
+++ src/main/java/com/xh/HiXiaoshuoserver/filter/CorsFilter.java
@@ -0,0 +1,10 @@
+package com.xh.HiXiaoshuoserver.filter;
+
+public class CorsFilter {
+}
+/**
+ * @program: HiXiaoshuo-server
+ * @description:
+ * @author: XuHao
+ * @create: 2018-12-12 11:12
+ **/
